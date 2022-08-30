ea0e7e4179657840dad628573f10f573df54fc78
mostafa jamali <m.jamali20274@gmail.com>
2022-08-30 10:33:46 +0430

    add two btn in to page

--- src/components/LegalRegister.vue
+++ src/components/LegalRegister.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="legal-register-page">
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field v-model="companyName" :rules="fieldRules" label="نام شرکت" required></v-text-field>
-      <v-text-field v-model="companyType" :rules="fieldRules" label="نوع شرکت" required></v-text-field>
-      <v-text-field v-model="nationalID" :rules="fieldRules" label="شناسه ملی" required></v-text-field>
-      <v-text-field v-model="email" :rules="emailRules" label="ایمیل" required></v-text-field>
-      <v-text-field v-model="phone" :rules="fieldRules" label="شماره تماس" required></v-text-field>
-      <v-text-field v-model="password" :rules="fieldRules" label="گذر واژه" required></v-text-field>
+    <v-form ref="form" v-model="valid" lazy-validation class="w-100">
+      <div class="mb-4">
+        <label for="companyName" class="d-block mb-3">نام شرکت :</label>
+        <input v-model="companyName" name="companyName" class="input-field" placeholder="نام شرکت" required />
+      </div>
+      <div class="mb-4">
+        <label for="companyType" class="d-block mb-3">نوع شرکت :</label>
+        <input v-model="companyType" name="companyType" class="input-field" placeholder="نوع شرکت" required />
+      </div>
+      <div class="mb-4">
+        <label for="nationalID" class="d-block mb-3">شناسه ملی :</label>
+        <input v-model="nationalID" name="nationalID" class="input-field" placeholder="شناسه ملی" required />
+      </div>
+      <div class="mb-4">
+        <label for="email" class="d-block mb-3"> ایمیل :</label>
+        <input v-model="email" name="email" class="input-field" placeholder="ایمیل" required />
+      </div>
+      <div class="mb-4">
+        <label for="phone" class="d-block mb-3"> شماره تماس :</label>
+        <input v-model="phone" name="phone" class="input-field" placeholder="شماره تماس" required />
+      </div>
+      <div class="mb-4">
+        <label for="password" class="d-block mb-3"> گذر واژه :</label>
+        <input v-model="password" name="password" class="input-field" placeholder="گذر واژه" required />
+      </div>
 
-      <v-btn color="primary" class="my-4" @click="resetValidation"> ایجاد کاربر </v-btn>
+      <!-- <v-text-field v-model="companyName" :rules="fieldRules" label="نام شرکت" required></v-text-field> -->
+
+      <v-btn color="primary" class="user-registration-btn my-4" @click="resetValidation"> ایجاد کاربر </v-btn>
       <p>قبلا ثبت نام کرده اید؟ <a href="/login">وارد شوید</a></p>
     </v-form>
   </div>
@@ -35,4 +55,23 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.legal-register-page {
+  width: 100%;
+  .input-field {
+    height: 50px;
+    width: 100%;
+    border-radius: 30px;
+    padding: 0.25rem 1.25rem 0.25rem 3rem;
+    box-shadow: 0 0 19px 1px rgb(149 152 160 / 18%);
+    border: 1px solid transparent;
+  }
+
+  .user-registration-btn{
+    width: 100%;
+    height: 50px !important;
+    border-radius: 30px;
+    background-image: -webkit-linear-gradient(0deg,#109792 1%,#47e33f 100%);
+  }
+}
+</style>
--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col outlined cols="12" sm="4">
-        <v-card class="pa-3">
+      <v-col outlined class="d-flex justify-center">
+        <v-card class="register-card">
+          <div class="d-flex">
+            <a href="#" class="login-btn login-btn-active text-center my-4">ورود</a>
+            <a href="#" class="registration-btn registration-btn-active text-center my-4">ثبت نام</a>
+          </div>
           <v-switch v-model="showMessages" :label="showMessages ? 'حقیقی' : 'حقوقی'"></v-switch>
           <legal-register />
         </v-card>
@@ -21,4 +25,32 @@ export default {
   }),
 };
 </script>
-<style lang=""></style>
+<style lang="scss">
+.register-card {
+  width: 450px;
+  padding: 2rem;
+  border-radius: 2rem !important;
+
+  .login-btn,
+  .registration-btn {
+    width: 50%;
+    height: 60px;
+    text-decoration: none;
+    border-radius: 30px 30px 30px 0;
+    background-color: #eceff5;
+    color: #90939a;
+    padding: 1rem;
+  }
+  .login-btn {
+    border-radius: 30px 30px 30px 0;
+  }
+  .registration-btn {
+    border-radius: 30px 30px 0 30px;
+  }
+  .login-btn-active,
+  .registration-btn-active {
+    background-image: -webkit-linear-gradient(0deg, #109792 1%, #47e33f 100%);
+    color: #fff
+  }
+}
+</style>

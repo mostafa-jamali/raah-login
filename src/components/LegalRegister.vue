833392d30cd52b1f9a3b159cf10841876f867019
mostafa jamali <m.jamali20274@gmail.com>
2022-08-30 09:13:19 +0430

    first commit of raah login page

---
+++ src/components/LegalRegister.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="legal-register-page">
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="companyName" :rules="fieldRules" label="نام شرکت" required></v-text-field>
+      <v-text-field v-model="companyType" :rules="fieldRules" label="نوع شرکت" required></v-text-field>
+      <v-text-field v-model="nationalID" :rules="fieldRules" label="شناسه ملی" required></v-text-field>
+      <v-text-field v-model="email" :rules="emailRules" label="ایمیل" required></v-text-field>
+      <v-text-field v-model="phone" :rules="fieldRules" label="شماره تماس" required></v-text-field>
+      <v-text-field v-model="password" :rules="fieldRules" label="گذر واژه" required></v-text-field>
+
+      <v-btn color="primary" class="my-4" @click="resetValidation"> ایجاد کاربر </v-btn>
+      <p>قبلا ثبت نام کرده اید؟ <a href="/login">وارد شوید</a></p>
+    </v-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LegalRegister',
+  data: () => ({
+    valid: true,
+    companyName: '',
+    companyType: '',
+    nationalID: '',
+    phone: '',
+    password: '',
+    fieldRules: [v => !!v || 'این فیلد الزامی می باشد.'],
+    email: '',
+    emailRules: [v => !!v || 'این فیلد الزامی می باشد.', v => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
+  }),
+
+  methods: {
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/pages/Register.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col outlined cols="12" sm="4">
+        <v-card class="pa-3">
+          <v-switch v-model="showMessages" :label="showMessages ? 'حقیقی' : 'حقوقی'"></v-switch>
+          <legal-register />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import LegalRegister from '../../components/LegalRegister.vue';
+
+export default {
+  name: 'Register',
+  components: { LegalRegister },
+  data: () => ({
+    showMessages: false,
+  }),
+};
+</script>
+<style lang=""></style>
